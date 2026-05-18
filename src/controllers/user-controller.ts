9911c6fcd627a8041d352a26436157b1a7e2c641
Matheus Silva <rematheussilva@gmail.com>
2026-05-18 16:44:36 -0500

    feat: add validate user data

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,7 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import { z } from "zod";
 
 export class UserController {
   create(request: Request, response: Response, next: NextFunction) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.string().email(),
+      password: z.string().min(8),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
     return response.json({ message: "Ok!" });
   }
 }
