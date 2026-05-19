91c7110fd12272adedd8b8f8cf837fbb38323a58
Matheus Silva <rematheussilva@gmail.com>
2026-05-19 14:48:16 -0500

    feat:create route and authentication controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { sessionsRoutes } from "./sessions-routes";
 import { userRoutes } from "./user-routes";
 
 const routes = Router();
 routes.use("/users", userRoutes);
+routes.use("/sessions", sessionsRoutes);
 
 export { routes };
