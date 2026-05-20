a5f21b6a5f50355f2aefca1a02015d724c4a8d02
Matheus Silva <rematheussilva@gmail.com>
2026-05-20 16:54:19 -0500

    feat: add controller and delivery log route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { deliveriesRoutes } from "./deliveries-routes";
+import { deliveryLogsRoutes } from "./delivery-logs-routes";
 import { sessionsRoutes } from "./sessions-routes";
 import { userRoutes } from "./user-routes";
 
@@ -7,5 +8,6 @@ const routes = Router();
 routes.use("/users", userRoutes);
 routes.use("/sessions", sessionsRoutes);
 routes.use("/deliveries", deliveriesRoutes);
+routes.use("/delivery-logs", deliveryLogsRoutes);
 
 export { routes };
