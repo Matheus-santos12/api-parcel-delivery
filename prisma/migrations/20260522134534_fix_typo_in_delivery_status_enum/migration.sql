2d3f4aeb057ebc69af3124e1322a0c17f4da979a
Matheus Silva <rematheussilva@gmail.com>
2026-05-22 08:48:38 -0500

    fix: correct typo in DeliveryStatus enum

---
+++ prisma/migrations/20260522134534_fix_typo_in_delivery_status_enum/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - The values [precessing] on the enum `DeliveryStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "DeliveryStatus_new" AS ENUM ('processing', 'shipped', 'delivered');
+ALTER TABLE "deliveries" ALTER COLUMN "status" DROP DEFAULT;
+ALTER TABLE "deliveries" ALTER COLUMN "status" TYPE "DeliveryStatus_new" USING ("status"::text::"DeliveryStatus_new");
+ALTER TYPE "DeliveryStatus" RENAME TO "DeliveryStatus_old";
+ALTER TYPE "DeliveryStatus_new" RENAME TO "DeliveryStatus";
+DROP TYPE "DeliveryStatus_old";
+ALTER TABLE "deliveries" ALTER COLUMN "status" SET DEFAULT 'processing';
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "deliveries" ALTER COLUMN "status" SET DEFAULT 'processing';
--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,5 +1,16 @@
+import { app } from "@/app";
+import request from "supertest";
+
 describe("UsersController", () => {
-  it("test", () => {
-    console.log("Passou e ja foi");
+  it("should create a new user successfully", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Jhollyfer",
+      email: "jhollyfer@example.com",
+      password: "password1234",
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.name).toBe("Jhollyfer");
   });
 });
